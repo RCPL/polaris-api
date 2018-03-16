87f47f50f2f8336a32f78b7a1a16d75f92cfb580
Alan <alan@atendesigngroup.com>
2018-03-16 21:08:51 +0100

    Adds new helper method for parameter class and cleans up formatting

--- src/Utility/Parameters.php
+++ src/Utility/Parameters.php
@@ -7,14 +7,24 @@ use Zend\Stdlib\Parameters as ZendParameters;
 
 class Parameters extends ZendParameters {
 
-    /**
-     * @param Parameters|array $parameters
-     *
-     * @return Parameters;
-     */
-    public function merge($parameters) {
-        $params = is_a($parameters, 'Parameters') ? $parameters->toArray() : $parameters;
-        $this->exchangeArray(ArrayUtils::merge($this->toArray(), $parameters));
-        return $this;
-    }
+  /**
+   * @param Parameters|array $parameters
+   *
+   * @return Parameters;
+   */
+  public function merge($parameters) {
+    $params = is_a($parameters, 'Parameters') ? $parameters->toArray() : $parameters;
+    $this->exchangeArray(ArrayUtils::merge($this->toArray(), $parameters));
+    return $this;
+  }
+
+  /**
+   * Creates a new instance of this object with no reference.
+   *
+   * @return Parameters;
+   */
+  public function clone() {
+    return new static($this->getArrayCopy());
+  }
+
 }
