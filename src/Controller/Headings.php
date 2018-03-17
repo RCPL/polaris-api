e00faec303780b43b3cdcf7440c8b335b2074d84
Alan <alan@atendesigngroup.com>
2018-03-17 07:10:18 +0100

    Updates headings object to make easier request command

--- src/Controller/Headings.php
+++ src/Controller/Headings.php
@@ -2,17 +2,58 @@
 
 namespace RCPL\Polaris\Controller;
 
+use RCPL\Polaris\Client;
+use RCPL\Polaris\Utility\Parameters;
+
 class Headings extends ControllerBase {
-  public function search($qualifierName = 'SU', array $params = []) {
-    $defaults = [
-      'startpoint' => '1',
+
+  /**
+   * @var Parameters
+   */
+  private $params;
+
+  public function __construct(Client $client) {
+    parent::__construct($client);
+    $this->params = new Parameters([
       'numterms' => '50',
       'preferredpos' => '1'
-    ];
+    ]);
+  }
+
+
+  public function author($text, array $params = []) {
+    return $this->text($text)->search('AU', $params);
+  }
+
+  public function series($text, array $params = []) {
+    return $this->text($text)->search('SE', $params);
+  }
+
+  public function subject($text, array $params = []) {
+    return $this->text($text)->search('SU', $params);
+  }
+
+  public function title($text, array $params = []) {
+    return $this->text($text)->search('TI', $params);
+  }
+
+  public function limit($numterms) {
+    $this->params->set('numterms', $numterms);
+    return $this;
+  }
+
+  private function text($text) {
+    $this->params->set('startpoint', $text);
+    return $this;
+  }
+
+  public function search($qualifierName = 'SU', array $params = []) {
+    $params = $this->params->merge($params);
     return $this->client->request()
       ->public()
+      ->simple('HeadingsSearchRows')
       ->path('search/headings/' . $qualifierName)
-      ->query(array_merge($defaults, $params))
+      ->query($params->toArray())
       ->get()
       ->send();
   }
