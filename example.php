a799117f066f55ac6b0adabe9c64fe5d7296bbb6
Alan <alan@atendesigngroup.com>
2018-03-23 00:53:17 +0100

    Updates documentation and adds kint as a dependency to use outside of Drupal

--- example.php
+++ example.php
@@ -23,19 +23,21 @@ print '<h1>Polaris API Example Calls</h1>';
 // Try pulling a list of 25 titles.
 print '<h2>1) Pulling a list of 25 titles w/ keyword "Harry Potter" using ' .
 'BibSearch</h2>';
-$result = $client->bibs->search(['text' => 'Harry Potter', 'total' => 25]);
+$result = $client->bibliography->search('Harry Potter', ['bibsperpage' => 25]);
+Kint::dump($result);
 
 // Get a list of hold requests for a customer.
-/*print '<h2>2) Get a list of hold requests for a customer using PatronHoldRequestsGet</h2>';
+print '<h2>2) Get a list of hold requests for a customer using PatronHoldRequestsGet</h2>';
 $patron_barcode = 20080104020258; // Customer's library card number
-$result = PolarisAPI::getPatronHoldRequests($patron_barcode);
-display_result($result);*/
+$patron = $client->patron->get($patron_barcode);
+$result = $patron->holdRequests();
+Kint::dump($result);
 
 // Get a list of branch locations and other patron details
-/*print '<h2>3) Get the customer\'s preferred branch location & "patron ID" using PatronSearch</h2>';
+print '<h2>3) Get the customer\'s preferred branch location & "patron ID" using PatronSearch</h2>';
 $patron_barcode = 20080104020258; // Customer's library card number
-$result = PolarisAPI::patronSearch($patron_barcode);
-display_result($result);*/
+$result = $client->patron->search($patron_barcode);
+Kint::dump($result);
 
 // Try putting something on hold.
 /*print '<h2>4) Put a copy of Harry Potter on request using HoldRequestCreate</h2>';
