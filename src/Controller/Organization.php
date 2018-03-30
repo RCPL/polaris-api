2d6d506688cb68537aa85e6e184204eedb521e04
Alan <alan@atendesigngroup.com>
2018-03-30 22:18:29 +0200

    Adds static caching for organizations/locations

--- src/Controller/Organization.php
+++ src/Controller/Organization.php
@@ -4,12 +4,62 @@ namespace RCPL\Polaris\Controller;
 
 class Organization extends ControllerBase {
 
+  protected $organizations;
+
+  /**
+   * @deprecated
+   *
+   * Use getAll() or getByType()
+   */
   public function get($type = 'all') {
     return $this->client->request()
       ->public()
       ->path('organizations/' . $type)
       ->get()
       ->send();
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getAll($type = 'all') {
+    if (!isset($this->organizations)) {
+      $this->organizations = $this->client->request()
+        ->public()
+        ->path('organizations/' . $type)
+        ->simple('OrganizationsGetRows')
+        ->get()
+        ->send();
+    }
+    return $this->organizations;
+  }
+
+  protected function getByKey($key, $name) {
+    foreach ($this->getAll() as $org) {
+      if ($org->{$key} == $name) {
+        return $org;
+      }
+    }
+    return FALSE;
+  }
+
+  public function getByAbbr($abbr) {
+    return $this->getByKey('Abbreviation', $abbr);
+  }
+
+  public function getByDisplayName($name) {
+    return $this->getByKey('DisplayName', $name);
+  }
+  public function getByName($name) {
+    return $this->getByKey('Name', $name);
+  }
+
+  public function getByCode($code) {
+    return $this->getByKey('OrganizationCodeID', $code);
+  }
+
+  public function getById($id) {
+    return $this->getByKey('OrganizationID', $id);
   }
 
 }
